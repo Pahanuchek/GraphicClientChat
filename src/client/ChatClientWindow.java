d225c088bf3b2b3277c57eb71b5062cd06822eb4
pavel-novozhilov <p.novozhilo24@mail.ru>
2024-11-08 16:57:48 +0300

    create chat client window

--- src/client/ChatClientWindow.java
+++ src/client/ChatClientWindow.java
@@ -1,12 +1,18 @@
 package client;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
 
 public class ChatClientWindow extends JFrame {
     private static final int WINDOW_HEIGHT = 400;
     private static final int WINDOW_WIDTH = 300;
     private static final int WINDOW_POSX = 100;
     private static final int WINDOW_POSY = 150;
+    private static final JPanel panel = new JPanel();
+    private final JTextArea text = new JTextArea();
+    private final JTextArea message = new JTextArea("");
+    private final JButton btnSend = new JButton("Send");
 
     public ChatClientWindow() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -14,5 +20,14 @@ public class ChatClientWindow extends JFrame {
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("Chat Client");
         setResizable(false);
+        message.setBorder(new BevelBorder(0, Color.BLACK, Color.GRAY));
+        message.setRows(3);
+        message.setColumns(20);
+
+        panel.add(message, BorderLayout.NORTH);
+        panel.add(btnSend, BorderLayout.SOUTH);
+
+        add(text, BorderLayout.CENTER);
+        add(panel, BorderLayout.SOUTH);
     }
 }
